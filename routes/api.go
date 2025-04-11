139f14c7997fbbdfb00472a8b891b9434c99f3c7
NoorUllah43 <noorullah382040.com>
2025-04-11 15:51:02 +0500

    Add analysis functionality to API; and sendback data

--- routes/api.go
+++ routes/api.go
@@ -3,7 +3,6 @@ package routes
 import (
 	"fmt"
 	"os"
-
 	"github.com/gofiber/fiber/v3"
 )
 
@@ -12,9 +11,6 @@ func Routes(app *fiber.App) {
 	app.Post("/uploadfile", filehandler)
 
 }
-
-
-
 
 func filehandler(ctx fiber.Ctx) error {
 
@@ -29,18 +25,54 @@ func filehandler(ctx fiber.Ctx) error {
 		return err
 	}
 
-	
 	readfile, ferr := os.ReadFile("./uploads/file.txt")
 	if ferr != nil {
 		panic(ferr)
 	}
-	
+
 	fileContent := string(readfile)
 
+	data := analyze(fileContent)
 
+	return ctx.JSON(data)
 
+}
 
+func analyze(str string) map[string]int {
 
+	chunkeData := make(map[string]int)
 
-	return ctx.JSON(fileContent)
+	for i := 0; i < len(str); i++ {
+		chunkeData["character"]++
+		if str[i] == (' ') {
+			chunkeData["spaces"]++
+		}
+		if str[i] == ('.') {
+			chunkeData["lines"]++
+		}
+		if str[i] == ('\n') {
+			chunkeData["paragraph"]++
+		}
+		if str[i] == (' ') || str[i] == ('\n') {
+			chunkeData["words"]++
+		}
+		if str[i] == (';') || str[i] == (':') || str[i] == ('\'') || str[i] == ('"') || str[i] == (',') || str[i] == ('.') || str[i] == ('?') {
+			chunkeData["punctuation"]++
+		}
+		if str[i] == ('[') || str[i] == (']') || str[i] == ('{') || str[i] == ('}') || str[i] == ('(') || str[i] == (')') || str[i] == ('|') || str[i] == ('\\') || str[i] == ('/') || str[i] == ('+') || str[i] == ('=') || str[i] == ('<') || str[i] == ('>') {
+			chunkeData["symboles"]++
+		}
+		if str[i] == ('!') || str[i] == ('@') || str[i] == ('#') || str[i] == ('$') || str[i] == ('%') || str[i] == ('^') || str[i] == ('&') || str[i] == ('*') || str[i] == ('~') {
+			chunkeData["specialCharacters"]++
+		}
+		if str[i] == ('0') || str[i] == ('1') || str[i] == ('2') || str[i] == ('3') || str[i] == ('4') || str[i] == ('5') || str[i] == ('6') || str[i] == ('7') || str[i] == ('8') || str[i] == ('9') {
+			chunkeData["digits"]++
+		}
+		if str[i] == ('a') || str[i] == ('e') || str[i] == ('i') || str[i] == ('o') || str[i] == ('u') {
+			chunkeData["vowels"]++
+		}
+	}
+
+	return chunkeData
+
 }
