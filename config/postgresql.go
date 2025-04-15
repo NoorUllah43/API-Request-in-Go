13394640d04d909c639f3cd49aff4f2778677b4b
NoorUllah43 <noorullah382040.com>
2025-04-15 15:51:13 +0500

    connect postgresql database

---
+++ config/postgresql.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func ConnectPostgresqlDB() {
+	connectionString := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=disable", os.Getenv("DB_HOST"),os.Getenv("DB_PORT"),os.Getenv("DB_USERNAME"),os.Getenv("DB_PASSWORD"),os.Getenv("fisrtDB"))
+	db, err := sql.Open("postgres", connectionString)
+
+	if err != nil {
+		fmt.Println("database not connecting error", err)
+	}
+
+	fmt.Println("Database connected")
+	
+	defer db.Close()
+}
--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
+
+	"github.com/NoorUllah43/API-Request-in-Go/config"
 	"github.com/NoorUllah43/API-Request-in-Go/routes"
 	"github.com/gofiber/fiber/v3"
-	
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	app := fiber.New()
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("loagin env err:",err)
+	}
+
+	config.ConnectPostgresqlDB()
 
 	
 	routes.Routes(app)
