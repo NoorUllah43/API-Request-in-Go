9e58e4607342d2199d3ad6eb5f6644f979016728
NoorUllah43 <noorullah382040.com>
2025-04-15 16:26:42 +0500

    correct database connection string

--- config/postgresql.go
+++ config/postgresql.go
@@ -9,7 +9,7 @@ import (
 )
 
 func ConnectPostgresqlDB() {
-	connectionString := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=disable", os.Getenv("DB_HOST"),os.Getenv("DB_PORT"),os.Getenv("DB_USERNAME"),os.Getenv("DB_PASSWORD"),os.Getenv("fisrtDB"))
+	connectionString := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=disable", os.Getenv("DB_HOST"),os.Getenv("DB_PORT"),os.Getenv("DB_USERNAME"),os.Getenv("DB_PASSWORD"),os.Getenv("DB_NAME"))
 	db, err := sql.Open("postgres", connectionString)
 
 	if err != nil {
